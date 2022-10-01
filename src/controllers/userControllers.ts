bfa0a76b93341791b44c740ff61f7699ab487522
Gadiel <gad.azevedo@gmail.com>
2022-10-01 20:47:47 -0300

    feat: create sign in function in user controllers

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import * as userServices from "../services/userServices";
+import { TLoginUser, TUserData } from "../types/userTypes";
 
 export async function signUp(req: Request, res: Response) {
-  const newUser = req.body;
+  const newUser: TUserData = req.body;
 
   await userServices.signUp(newUser);
   res.status(201).send("User registered successfully");
+}
+
+export async function signIn(req: Request, res: Response) {
+  const userData: TLoginUser = req.body;
+
+  //service
+  res.status(200); //send token
 }
