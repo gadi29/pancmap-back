4b79a7106bac2898d16dc3260c7be78bc1ac9df7
Gadiel <gad.azevedo@gmail.com>
2022-10-01 20:10:30 -0300

    feat: import signup function in user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { signUp } from "../controllers/userControllers";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
 import { newUserSchema } from "../schemas/userSchemas";
 
 const userRouter = Router();
 
-userRouter.post("/signup", validateSchemaMiddleware(newUserSchema));
+userRouter.post("/signup", validateSchemaMiddleware(newUserSchema), signUp);
 userRouter.post("/signin");
 
 export default userRouter;
