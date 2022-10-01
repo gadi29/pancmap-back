2c6b4a62516718d5691a52b37cd1a2d6fbcf7605
Gadiel <gad.azevedo@gmail.com>
2022-10-01 19:41:06 -0300

    feat: validate new user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
+import { newUserSchema } from "../schemas/userSchemas";
 
 const userRouter = Router();
 
-userRouter.post("/signup");
+userRouter.post("/signup", validateSchemaMiddleware(newUserSchema));
 userRouter.post("/signin");
 
 export default userRouter;
