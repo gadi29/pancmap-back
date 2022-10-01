e7b4a6472f2a229cca41a0f37106f2b6009f6665
Gadiel <gad.azevedo@gmail.com>
2022-10-01 20:07:28 -0300

    feat: user type in schema

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -1,6 +1,7 @@
 import Joi from "joi";
+import { TUserData } from "../types/userTypes";
 
-export const newUserSchema = Joi.object({
+export const newUserSchema = Joi.object<TUserData>({
   name: Joi.string().trim().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
