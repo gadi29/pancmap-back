05e42e12a2fd2dcfa79aa21be0688647f7e7d26b
Gadiel <gad.azevedo@gmail.com>
2022-10-01 21:10:42 -0300

    feat: finish login route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { signUp } from "../controllers/userControllers";
+import { signIn, signUp } from "../controllers/userControllers";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
 import { loginSchema, newUserSchema } from "../schemas/userSchemas";
 
 const userRouter = Router();
 
 userRouter.post("/signup", validateSchemaMiddleware(newUserSchema), signUp);
-userRouter.post("/signin", validateSchemaMiddleware(loginSchema));
+userRouter.post("/signin", validateSchemaMiddleware(loginSchema), signIn);
 
 export default userRouter;
