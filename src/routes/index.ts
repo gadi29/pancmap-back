9615ca65b8226a4416119cb816f1d058cd93dfa2
Gadiel <gad.azevedo@gmail.com>
2022-10-02 09:54:42 -0300

    feat: create specie router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import specieRouter from "./specieRoutes";
 import userRouter from "./userRoutes";
 
 const router = Router();
 
 router.use(userRouter);
+router.use(specieRouter);
 
 export default router;
---
+++ src/routes/specieRoutes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const specieRouter = Router();
+
+export default specieRouter;
