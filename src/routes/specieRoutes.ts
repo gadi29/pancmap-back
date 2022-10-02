9eb97a25e64c4cdfc59dc4a11d1743526d861442
Gadiel <gad.azevedo@gmail.com>
2022-10-02 10:01:11 -0300

    feat: create specie routes

--- src/routes/specieRoutes.ts
+++ src/routes/specieRoutes.ts
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const specieRouter = Router();
 
+specieRouter.post("/specie");
+specieRouter.get("/specie/:id");
+specieRouter.get("/species");
+specieRouter.put("/specie");
+specieRouter.delete("/specie/:id");
+
 export default specieRouter;
